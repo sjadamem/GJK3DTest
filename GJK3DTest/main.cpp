fb84260a66b473284fa7a84638fd1c540ba55aeb
Sebastian J Adame I <sjadamem@gmail.com>
2021-05-31 00:42:35 -0700

    update vec3 struct with operator overloading

--- GJK3DTest/main.cpp
+++ GJK3DTest/main.cpp
@@ -11,6 +11,7 @@ void GJK();
 
 struct vec3
 {
+public:
 	float x, y, z;
 
 	vec3(float x, float y, float z) : x(x), y(y), z(z) {};
@@ -20,6 +21,69 @@ struct vec3
 		output << "X : " << v.x << "|Y : " << v.y << "|Z : " << v.z;
 		return output;
 	}
+	
+	vec3 operator+ (const vec3& val)
+	{
+		return add(val);
+	}
+
+	vec3 operator- (const vec3& val)
+	{
+		return sub(val);
+	}
+
+	vec3 operator* (const float& val)
+	{
+		return mul(val);
+	}
+
+	vec3 operator/ (const float& val)
+	{
+		return div(val);
+	}
+
+	vec3 operator- ()
+	{
+		return vec3(-x, -y, -z);
+	}
+
+	static float dot(vec3& a, vec3& b)
+	{
+		return a.x * b.x + a.y * b.y + a.z * b.z;
+	}
+
+	static vec3 cross(vec3& a, vec3& b)
+	{
+		return vec3(
+			a.y * b.z - a.z * b.y, 
+			a.z * b.x - a.x * b.z, 
+			a.x * b.y - a.y * b.x
+		);
+	}
+
+private:
+	
+	vec3 add(vec3 val)
+	{
+		return vec3(x + val.x, y + val.y, z + val.z);
+	}
+
+	vec3 sub(vec3 val)
+	{
+		return vec3(x - val.x, y - val.y, z - val.z);
+	}
+
+	template <typename T>
+	vec3 mul(T val)
+	{
+		return vec3(x * val, y * val, z * val);
+	}
+
+	template <typename T>
+	vec3 div(T val)
+	{
+		return vec3(x / val, y / val, z / val);
+	}
 };
 
 void GJK()
@@ -28,7 +92,8 @@ void GJK()
 	std::chrono::duration<float> elapsed_seconds = start - now;
 	float sec = elapsed_seconds.count();
 	
-	vec3(cos((float(rand()))), sin((float(rand()))), 0.0f);
+	float val = (float)rand();
+	cout << val << "\n" << vec3(cos(val), sin(val), 0.0f) << endl;
 }
 
 int main()
@@ -36,9 +101,34 @@ int main()
 	vector<vec3> square;
 	vector<vec3> triangle;
 
-	vec3 v(10.0f);
-	cout << v << endl;
+	square = {
+		vec3(-0.5f, -0.5f, 0.0f),
+		vec3( 0.5f, -0.5f, 0.0f),
+		vec3( 0.5f,  0.5f, 0.0f),
+		vec3(-0.5f,  0.5f, 0.0f)
+	};
 
-	system("pause");
+	triangle = {
+		vec3( 0.0f, -0.5f, 0.0f),
+		vec3( 1.0f, -0.5f, 0.0f),
+		vec3( 0.0f,  0.5f, 0.0f)
+	};
+
+	vec3 v(10.0f);
+	vec3 a = v;
+	a = -a;
+	a = (a - v) / 5;
+
+	cout << a << endl;
+
+
+	vec3 x(3.0f, 2.0f, 1.0f);
+	vec3 y(4.0f);
+
+	cout << vec3::dot(x, y) << endl;
+	cout << vec3::cross(x, y) << endl;
+
+	GJK();
+	
 	return 0;
 }
