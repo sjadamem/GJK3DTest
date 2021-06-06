2f48d061462fa423dcc7fc7bf1c91904e42830b0
Sebastian J Adame I <sjadamem@gmail.com>
2021-06-06 14:51:43 -0700

    2D GJK works well. 3D in progress

--- GJK3DTest/main.cpp
+++ GJK3DTest/main.cpp
@@ -4,10 +4,14 @@
 #include <time.h>
 #include <vector>
 #include <cmath>
+#include <map>
+#include <list>
 
+#include <glm/glm.hpp>
+#include <glm/vec3.hpp>
 
 using namespace std;
-
+/*
 struct vec3
 {
 public:
@@ -27,7 +31,25 @@ public:
 		return add(val);
 	}
 
+	vec3& operator+= (const vec3& val)
+	{
+		this->x += val.x;
+		this->y += val.y;
+
+		return *this;
+	}
+
 	vec3 operator- (const vec3& val)
+	{
+		return sub(val);
+	}
+
+	vec3 operator- ()
+	{
+		return vec3(-x, -y, -z);
+	}
+	
+	vec3 operator-= (const vec3& val)
 	{
 		return sub(val);
 	}
@@ -42,9 +64,23 @@ public:
 		return div(val);
 	}
 
-	vec3 operator- ()
+
+	vec3 operator= (vec3 val)
 	{
-		return vec3(-x, -y, -z);
+		return val;
+	}
+
+	nullptr_t operator= (nullptr_t val)
+	{
+		return val;
+	}
+
+	bool operator== (vec3& val)
+	{
+		if (this->x == val.x && this->y == val.y && this->z == val.z)
+			return true;
+
+		return false;
 	}
 
 	static float dot(vec3 a, vec3 b)
@@ -75,18 +111,18 @@ private:
 	
 	vec3 add(vec3 val)
 	{
-		return vec3(x + val.x, y + val.y, z + val.z);
+		return vec3(this->x + val.x, this->y + val.y, this->z + val.z);
 	}
 
 	vec3 sub(vec3 val)
 	{
-		return vec3(x - val.x, y - val.y, z - val.z);
+		return vec3(this->x - val.x, this->y - val.y, this->z - val.z);
 	}
 
 	template <typename T>
 	vec3 mul(T val)
 	{
-		return vec3(x * val, y * val, z * val);
+		return vec3(this->x * val, this->y * val, this->z * val);
 	}
 
 	template <typename T>
@@ -95,133 +131,237 @@ private:
 		return vec3(x / val, y / val, z / val);
 	}
 };
-
-/*
-struct shape
-{
-	vec3 transform;
-
-	vector<vec3> points;
-
-	shape(vec3* points, vec3 trans = vec3()) : transform(trans) { this->points.push_back(*points); }
-
-};
 */
 
-void GJK(vector<vec3>& shapeA, vector<vec3>& shapeB);
-vec3 supportFunction(vec3 dir, vector<vec3>& shape);
-
-void GJK(vector<vec3>& shapeA, vector<vec3>& shapeB)
+struct shape
 {
-//	auto now = chrono::system_clock::now();
-//	std::chrono::duration<float> elapsed_seconds = start - now;
-//	float sec = elapsed_seconds.count();
-	
-	//Get a random direction vector
-	float val = (float)(rand() % 360);
-	cout << "Random degrees::\n" << val << endl;
-	val *= M_PI / 180.0f;
-	vec3 dir(cos(val), sin(val), 0.0f);
-	cout << "Random direction::\n" << dir << endl << endl;
+	glm::vec3 mCenter;
 
-	//Find a point on both shapes A and B that each maximize the direction vector
-	vec3 furthestShapePointA = supportFunction(dir, shapeA);
-	//For Minkowski difference, invert the direction vector for Shape B
-	vec3 furthestShapePointB = supportFunction(-dir, shapeB);
-	cout << "Shape A::\n" << furthestShapePointA << endl;
-	cout << "Shape B::\n" << furthestShapePointB << endl << endl;
+	glm::vec3* mPoints;
+	unsigned int mSize;
 
-	//Our point on the simplex will be the difference between both points
-	vec3 simplexPointA = furthestShapePointA - furthestShapePointB;
-	cout << "Simplex Point A::\n" << simplexPointA << endl << endl;
+	shape(glm::vec3* points, unsigned int size, glm::vec3 trans = glm::vec3(0.0f)) 
+		: mSize(size), mCenter(glm::vec3(0.0f))
+	{ 
+		mPoints = move(points); 
+		translate(trans);
+	}
 
-	dir = vec3() - simplexPointA;
-	cout << "Direction to Origin::\n" << dir << endl;
-
-	furthestShapePointA = supportFunction(dir, shapeA);
-	furthestShapePointB = supportFunction(-dir, shapeB);
-	cout << "Shape A::\n" << furthestShapePointA << endl;
-	cout << "Shape B::\n" << furthestShapePointB << endl << endl;
-
-	vec3 simplexPointB = furthestShapePointA - furthestShapePointB;
-	cout << "Simplex Point B::\n" << simplexPointB << endl << endl;
-
-	vec3 dirAO = -simplexPointA;
-	vec3 dirAB = simplexPointB - simplexPointA;
-	
-	vec3 ABxAO = vec3::cross(dirAB, dirAO);
-	dir = vec3::cross(vec3::cross(dirAB, dirAO), dirAB);
-
-	furthestShapePointA = supportFunction(dir, shapeA);
-	furthestShapePointB = supportFunction(-dir, shapeB);
-	cout << "Shape A::\n" << furthestShapePointA << endl;
-	cout << "Shape B::\n" << furthestShapePointB << endl << endl;
-
-	vec3 simplexPointC = furthestShapePointA - furthestShapePointB;
-	cout << "Simplex Point C::\n" << simplexPointC << endl << endl;
-
-	vec3 dirAC = simplexPointC - simplexPointA;
-
-	vec3 ABperp = vec3::cross(vec3::cross(dirAB, dirAC), dirAC);
-	vec3 ACperp = vec3::cross(vec3::cross(dirAC, dirAB), dirAB);
-
-	bool abCheck = vec3::dot(ABperp, dirAO) < 0.0f;
-	bool acCheck = vec3::dot(ACperp, dirAO) < 0.0f;
-
-	if (abCheck && abCheck)
-		cout << "ORIGIN IN SIMPLEX" << endl << endl;
-}
-
-vec3 supportFunction(vec3 dir, vector<vec3>& shape)
-{
-	float maxDotProd = vec3::dot(dir, shape[0]);
-	vec3 furthestPoint = shape[0];
-
-	for (unsigned int i = 1; i < shape.size(); i++)
+	void translate(glm::vec3& trans)
 	{
-		float val = vec3::dot(dir, shape[i]);
-		if (val > maxDotProd)
+		for (unsigned int i = 0; i < mSize; i++)
+			mPoints[i] += trans;
+
+		mCenter += trans;
+	}
+
+	glm::vec3 furthestPoint(glm::vec3 dir)
+	{
+		float maxDotProd = glm::dot(dir, mPoints[0]);
+		glm::vec3 furthestPoint = mPoints[0];
+
+		for (unsigned int i = 1; i < mSize; i++)
 		{
-			furthestPoint = shape[i];
-			maxDotProd = val;
+			float val = glm::dot(dir, mPoints[i]);
+			if (val > maxDotProd)
+			{
+				furthestPoint = mPoints[i];
+				maxDotProd = val;
+			}
+		}
+
+		return furthestPoint;
+	}
+};
+
+struct simplex
+{
+public:
+	simplex() : mSize(3), mLength(0)
+	{ 
+		mPoints = new glm::vec3[mSize];
+	}
+
+	glm::vec3 operator[] (unsigned int index)
+	{
+		return mPoints[index];
+	}
+
+	void append(glm::vec3 val)
+	{
+		if (mLength == mSize)
+			return;
+
+		mPoints[mLength] = val;
+		mLength++;
+	}
+
+	void remove(glm::vec3 val)
+	{
+		for (unsigned int i = 0; i < mLength; i++)
+		{
+			if (mPoints[i] == val)
+			{
+				if (i < mLength - 1)
+					move(mPoints + i + 1, mPoints + mLength, mPoints + i);
+
+				mLength--;
+				break;
+			}
 		}
 	}
+
+	unsigned int length()
+	{
+		return mLength;
+	}
+
+private:
+	
+	glm::vec3* mPoints;
+	unsigned int mSize;
+	unsigned int mLength;
+};
+
+bool GJK(shape& shapeA, shape& shapeB);
+glm::vec3 supportFunction(shape& shapeA, shape& shapeB, glm::vec3& dir);
+
+bool handleSimplex(simplex& simplex, glm::vec3* dir);
+bool lineCase(simplex& simplex, glm::vec3* dir);
+bool triangleCase(simplex& simplex, glm::vec3* dir);
+
+bool GJK(shape& shapeA, shape& shapeB)
+{
+	simplex simplex;
+	glm::vec3 dir;
+
+	//Start with a random direction
+	dir = glm::normalize(shapeB.mCenter - shapeA.mCenter);
+	simplex.append(supportFunction(shapeA, shapeB, dir));
+
+	dir = -simplex[0];
+
+	while (true)
+	{
+		glm::vec3 vec = supportFunction(shapeA, shapeB, dir);
+		if (glm::dot(vec, dir) < 0.0f)
+			return false;
+
+		simplex.append(vec);
 		
-	return furthestPoint;
+		if (handleSimplex(simplex, &dir))
+			return true;
+	}
+}
+
+glm::vec3 supportFunction(shape& shapeA, shape& shapeB, glm::vec3& dir)
+{
+	return shapeA.furthestPoint(dir) - shapeB.furthestPoint(-dir);
+}
+
+bool handleSimplex(simplex& simplex, glm::vec3* dir)
+{
+	if (simplex.length() == 2)
+		return lineCase(simplex, dir);
+
+	return triangleCase(simplex, dir);
+}
+
+bool lineCase(simplex& simplex, glm::vec3* dir)
+{
+	glm::vec3* A = &simplex[0];
+	glm::vec3* B = &simplex[1];
+
+	glm::vec3 AB = *B - *A;
+	glm::vec3 AO = glm::vec3(0.0f) - *A;
+
+	*dir = glm::normalize(glm::cross(glm::cross(AB, AO), AB));
+
+	float slope = (B->y - A->y) / (B->x - A->x);
+	if (slope * B->x + B->y == 0.0f)
+		return true;
+
+	return false;
+}
+
+bool triangleCase(simplex& simplex, glm::vec3* dir)
+{
+	glm::vec3* A = &simplex[0];
+	glm::vec3* B = &simplex[1];
+	glm::vec3* C = &simplex[2];
+
+	glm::vec3 AB = *B - *A;
+	glm::vec3 AC = *C - *A;
+	glm::vec3 AO = glm::vec3(0.0f) - *A;
+
+	glm::vec3 ABperp = glm::normalize(glm::cross(glm::cross(AC, AB), AB));
+	glm::vec3 ACperp = glm::normalize(glm::cross(glm::cross(AB, AC), AC));
+
+	if (glm::dot(ABperp, AO) > 0.0f)
+	{
+		simplex.remove(*C);
+		*dir = ABperp;
+		return false;
+	}
+	else if (glm::dot(ACperp, AO) > 0.0f)
+	{
+		simplex.remove(*B);
+		*dir = ACperp;
+		return false;
+	}
+
+	return true;
 }
 
 int main()
 {
 	srand((unsigned int)time(NULL));
-
-	vec3 v(10.0f);
-	vec3 a = v;
-	a = -a;
-	a = (a - v) / 5;
-
-	cout << a << endl;
-
-	vec3 x(3.0f, 2.0f, 1.0f);
-	vec3 y(4.0f);
-
-	cout << vec3::dot(x, y) << endl;
-	cout << vec3::cross(x, y) << endl;
-
 	
-	vector<vec3> square = {
-		vec3(-1.0f,-1.0f, 0.0f),
-		vec3( 1.0f,-1.0f, 0.0f),
-		vec3( 1.0f, 1.0f, 0.0f),
-		vec3(-1.0f, 1.0f, 0.0f)
+	vector<glm::vec3> pointsA = {
+		glm::vec3(-1.0f,-1.0f, 0.0f),
+		glm::vec3( 1.0f,-1.0f, 0.0f),
+		glm::vec3( 1.0f, 1.0f, 0.0f),
+		glm::vec3(-1.0f, 1.0f, 0.0f)
 	};
 
-	vector<vec3> triangle = {
-		vec3(0.5f, -0.5f, 0.0f),
-		vec3(2.5f, -0.5f, 0.0f),
-		vec3(1.5f,  0.5f, 0.0f)
+	vector<glm::vec3> pointsB = {
+		glm::vec3(-0.1f, -0.5f, 0.0f),
+		glm::vec3( 0.1f, -0.5f, 0.0f),
+		glm::vec3( 0.0f,  0.5f, 0.0f)
 	};
 	
-	GJK(square, triangle);
-	
+	shape square(pointsA.data(), pointsA.size());
+	shape triangle(pointsB.data(), pointsB.size(), glm::vec3(1.0f, 0.1f, 0.0f));
+
+	unsigned int counter = 0;
+	glm::vec3 change(0.01, 0.0f, 0.0f);
+
+	glm::vec3 data[] = { glm::vec3(), glm::vec3(1.0f), glm::vec3(2.0f), glm::vec3(3.0f), glm::vec3(4.0f), glm::vec3(5.0f), glm::vec3(6.0f), glm::vec3(7.0f) };
+	glm::vec3* a = new glm::vec3[8];
+	a = move(data);
+
+	while (counter < 3000)
+	{
+		if (triangle.mCenter.x >= 2.5f || triangle.mCenter.x <= -2.5f)
+			change = -change;
+
+		cout << "Triangle Center\n" << triangle.mCenter.x << "\t" << triangle.mCenter.y << endl;
+		for (unsigned int i = 0; i < triangle.mSize; i++)
+			cout << triangle.mPoints[i].x << "::" << triangle.mPoints[i].y << "\t";
+
+		cout << "\nSquare Center\n" << square.mCenter.x << "\t" << square.mCenter.y << endl;
+		for (unsigned int i = 0; i < square.mSize; i++)
+			cout << square.mPoints[i].x << "::" << square.mPoints[i].y << "\t";
+
+		if (GJK(square, triangle))
+			cout << "Collision detected!!";
+
+		cout << endl;
+
+		triangle.translate(change);
+		counter++;
+	}
+//	cout << (char)('A' + 1) << endl;
+
 	return 0;
 }
